b5b049240701f95774bd692426d127607134d9a9
Henri Delozanne <hnr.delozanne@gmail.com>
2020-03-31 09:14:28 +0200

    Add nav and footer.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
   <v-app>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1>Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username" prepend-icon="mdi-account-circle"/>
-          <v-text-field label="Password"
-                        prepend-icon="mdi-lock"
-                        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                        :type="showPassword ? 'text' : 'password'"
-                        @click:append="showPassword = !showPassword"/>
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-app-bar app dense color="primary" dark>
+      <v-toolbar-title>My Vuetify application</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn text rounded>
+        Home
+      </v-btn>
+
+      <v-btn text rounded>
+        Login
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1>Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle"/>
+            <v-text-field label="Password"
+                          prepend-icon="mdi-lock"
+                          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                          :type="showPassword ? 'text' : 'password'"
+                          @click:append="showPassword = !showPassword"/>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+    <v-footer color="primary lighten-1" padless>
+      <v-layout justify-center wrap>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-flex primary lighten-2 py-4 text-center white--text xs12>
+          {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
 
